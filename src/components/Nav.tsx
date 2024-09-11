4d92da561d701a4bee4d423cde90ea4149152308
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-12 11:17:31 +1200

    add shadcn button component and appl on nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,33 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { Button } from '@/components/ui/button'
+
 import { IsAuthenticated } from './IsAuthenticated'
-import LoginButton from './LoginButon'
-import LogoutButton from './LogoutButton'
 import { NotAuthenticated } from './NotAuthenticated'
 import { Title } from './Title'
 
 export default function Nav() {
+  const { logout, loginWithRedirect } = useAuth0()
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+
+  const handleSignOut = () => {
+    logout()
+  }
+
   return (
     <div>
       <Title title="Tag Hunter"></Title>
       <NotAuthenticated>
-        <LoginButton />
+        <Button variant="outline" onClick={handleSignIn}>
+          Sign In
+        </Button>
       </NotAuthenticated>
       <IsAuthenticated>
-        <LogoutButton />
+        <Button variant="outline" onClick={handleSignOut}>
+          Sign Out
+        </Button>
       </IsAuthenticated>
     </div>
   )
