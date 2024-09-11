2f38acf7b6e8376d5483d91efc4473b7bd3f71d6
TheFlyingDeutchMan <tim.gerschner.nz@gmail.com>
2024-09-12 10:30:59 +1200

    added white brickwall background

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,13 +2,15 @@ import { Link } from 'react-router-dom'
 // import Nav from './Nav'
 function Home() {
     return (
-        <div>
+        <div className=''>
             <div className="content">
                 <h2>Get Started!</h2>
-                    <Link to="/game">Game Start</Link> {/* Just change the 'Link to="/game" whenever' */}
+                    <Link to="/game">Game Start</Link> {/* Just change the 'Link to="/game" to whatever is needed' */}
             </div>
         </div>
     )
 }
 
 export default Home
+
+// https://tailwindcss.com/docs/background-image
