f38bdbe0cb570493c9b9f7028e4bbd881be3a4b8
TheFlyingDeutchMan <tim.gerschner.nz@gmail.com>
2024-09-12 10:53:04 +1200

    font added to tailwind

---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+export function Title() {
+    return( 
+        <div>
+            <h1 className="font-title">Title</h1>
+        </div>
+    )
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,27 @@
 export default function Home() {
-  return <div>Home</div>
+  return ( 
+    <div className="font-title">Home</div>
+  )
 }
+
+
+
+
+
+
+// import { Link } from 'react-router-dom'
+// // import Nav from './Nav'
+// function Home() {
+//     return (
+//         <div>
+//             <div className="content">
+//                 <h2>Get Started!</h2>
+//                     <Link to="/game">Game Start</Link> {/* Just change the 'Link to="/game" to whatever is needed' */}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Home
+
+// // https://tailwindcss.com/docs/background-image
