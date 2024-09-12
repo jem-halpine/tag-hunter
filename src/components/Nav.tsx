67923b8c92d3f18826209a4b1d5430c2d7bfc817
jem-halpine <29801095+jem-halpine@users.noreply.github.com>
2024-09-13 10:30:56 +1200

    remove not used hooks

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,15 @@
-import { useAddUser, useUser } from '@/hooks/useUsers'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Link } from 'react-router-dom'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+
 import { IsAuthenticated } from './IsAuthenticated'
 import { NotAuthenticated } from './NotAuthenticated'
 import { Title } from './Title'
@@ -25,43 +36,43 @@ export default function Nav() {
             Sign In
           </Button>
         </NotAuthenticated>
-        
-      <div className="flex items-center">
-        <IsAuthenticated>
-          <Avatar className="mr-2">
-            <AvatarImage src={user?.picture} alt="user profile"></AvatarImage>
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button variant="default">
-                {user?.nickname}{' '}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                  />
-                </svg>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>
-                <Link to="/profile">Profile</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={handleSignOut}>
-                Sign Out
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </IsAuthenticated>
+
+        <div className="flex items-center">
+          <IsAuthenticated>
+            <Avatar className="mr-2">
+              <AvatarImage src={user?.picture} alt="user profile"></AvatarImage>
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button variant="default">
+                  {user?.nickname}{' '}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link to="/profile">Profile</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={handleSignOut}>
+                  Sign Out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </IsAuthenticated>
         </div>
       </div>
     </div>
