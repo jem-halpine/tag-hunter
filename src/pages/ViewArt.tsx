6d63ef50c3f2baaf3b89eb060651032ece2e1b10
Alexander Clark <alexanderclarknz@gmail.com>
2024-09-13 11:29:35 +1200

    style: format page to have pazaz

--- src/pages/ViewArt.tsx
+++ src/pages/ViewArt.tsx
@@ -7,7 +7,8 @@ import {
   Map,
   Pin,
 } from '@vis.gl/react-google-maps'
-import { LatLng } from 'models/models'
+import { Button } from '@/components/ui/button'
+import { Link } from 'react-router-dom'
 
 export default function ViewArt() {
   const { id: artworkID } = useParams()
@@ -31,34 +32,44 @@ export default function ViewArt() {
   console.log(artwork)
   const artPos = { lat: artwork.latitude, lng: artwork.longitude }
   return (
-    <div className="">
-      <img
-        className="w-[30vw] min-w-[200px]  rounded-md shadow-md"
-        src={`/images/${artwork.imageUrl}`}
-        alt=""
-      />
-      <p>Artist: {artwork.artist ? artwork.artist : 'Unknown Artist'}</p>
-      <p>Street Address: {artwork.location}</p>
-      <div className="size-[30vw] max-h-[45vh] border-2 border-thGray shadow-md">
-        <APIProvider apiKey={'AIzaSyAniaK3l1jH7gSgpiNd-PyBMB0ygsy8QXA'}>
-          <Map
-            defaultCenter={artPos}
-            defaultZoom={13}
-            mapId="gameMap"
-            minZoom={14}
-            fullscreenControl={null}
-          >
-            <AdvancedMarker
-              position={{
-                lat: artwork.latitude,
-                lng: artwork.longitude,
-              }}
+    <div id="container" className="flex w-full flex-col items-center p-10">
+      <div className="flex w-full flex-wrap justify-evenly">
+        <img
+          className="w-[30vw] min-w-[200px]  rounded-md shadow-md"
+          src={`/images/${artwork.imageUrl}`}
+          alt=""
+        />
+        <div className="size-[30vw] max-h-[45vh] border-2 border-thGray shadow-md">
+          <APIProvider apiKey={'AIzaSyAniaK3l1jH7gSgpiNd-PyBMB0ygsy8QXA'}>
+            <Map
+              defaultCenter={artPos}
+              defaultZoom={13}
+              mapId="gameMap"
+              minZoom={14}
+              fullscreenControl={null}
             >
-              <Pin background={'red'} borderColor={'black'} />
-            </AdvancedMarker>
-          </Map>
-        </APIProvider>
+              <AdvancedMarker
+                position={{
+                  lat: artwork.latitude,
+                  lng: artwork.longitude,
+                }}
+              >
+                <Pin background={'red'} borderColor={'black'} />
+              </AdvancedMarker>
+            </Map>
+          </APIProvider>
+        </div>
       </div>
+      <p className="text-lg">
+        <span className="font-bold">Artist:</span>{' '}
+        {artwork.artist ? artwork.artist : 'Unknown Artist'}
+      </p>
+      <p className="text-lg">
+        <span className="font-bold">Street Address:</span> {artwork.location}
+      </p>
+      <Link to="/gallery" className="mt-3">
+        <Button>Back</Button>
+      </Link>
     </div>
   )
 }
