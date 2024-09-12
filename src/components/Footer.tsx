13088d14a2c476b03f75e53c62bddb36de48f25b
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-12 14:30:51 +1200

    add icons to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,24 @@
 import { MdCopyright } from 'react-icons/md'
+import { FaFacebook } from 'react-icons/fa'
+import { FaDiscord } from 'react-icons/fa6'
+import { FaInstagram } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
 
 export default function Footer() {
+  //  TODO: add icon links
+
   return (
-    <div>
-      <MdCopyright />
-      <p>2024 TAG HUNTER</p>
+    <div className="flex justify-between">
+      <div className="flex items-center justify-center">
+        <MdCopyright />
+        <p className="text-xs">2024 TAG HUNTER</p>
+      </div>
+      <div className="flex">
+        <FaFacebook />
+        <FaDiscord />
+        <FaInstagram />
+        <FaTwitter />
+      </div>
     </div>
   )
 }
