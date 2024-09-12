eff81bad678dbc1f76fec0a6f26e90b70fd2c82e
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-12 13:29:11 +1200

    add play button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="flex items-center justify-center">
+      <Button variant="default" size="lg">
+        Play Now
+      </Button>
+    </div>
+  )
 }
-
-// import { Link } from 'react-router-dom'
-// // import Nav from './Nav'
-// function Home() {
-//     return (
-//         <div>
-//             <div className="content">
-//                 <h2>Get Started!</h2>
-//                     <Link to="/game">Game Start</Link> {/* Just change the 'Link to="/game" to whatever is needed' */}
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Home
-
-// // https://tailwindcss.com/docs/background-image
