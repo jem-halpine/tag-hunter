da796a90994a4135fa4d4e64dcb4aac9c589e3ff
Alexander Clark <alexanderclarknz@gmail.com>
2024-09-13 11:01:50 +1200

    feat: Add map functionality

--- src/pages/ViewArt.tsx
+++ src/pages/ViewArt.tsx
@@ -1,9 +1,17 @@
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { getArtworkById } from '../apis/artworks'
+import {
+  AdvancedMarker,
+  APIProvider,
+  Map,
+  Pin,
+} from '@vis.gl/react-google-maps'
+import { LatLng } from 'models/models'
 
 export default function ViewArt() {
   const { id: artworkID } = useParams()
+
   const {
     data: artwork,
     isPending,
@@ -21,6 +29,7 @@ export default function ViewArt() {
     return <>Error</>
   }
   console.log(artwork)
+  const artPos = { lat: artwork.latitude, lng: artwork.longitude }
   return (
     <div className="">
       <img
@@ -30,6 +39,26 @@ export default function ViewArt() {
       />
       <p>Artist: {artwork.artist ? artwork.artist : 'Unknown Artist'}</p>
       <p>Street Address: {artwork.location}</p>
+      <div className="size-[30vw] max-h-[45vh] border-2 border-thGray shadow-md">
+        <APIProvider apiKey={'AIzaSyAniaK3l1jH7gSgpiNd-PyBMB0ygsy8QXA'}>
+          <Map
+            defaultCenter={artPos}
+            defaultZoom={13}
+            mapId="gameMap"
+            minZoom={14}
+            fullscreenControl={null}
+          >
+            <AdvancedMarker
+              position={{
+                lat: artwork.latitude,
+                lng: artwork.longitude,
+              }}
+            >
+              <Pin background={'red'} borderColor={'black'} />
+            </AdvancedMarker>
+          </Map>
+        </APIProvider>
+      </div>
     </div>
   )
 }
