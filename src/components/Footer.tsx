96c5077f9a6968544a062b1fb24cdbf5ae4c4d0e
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-12 13:42:39 +1200

    install icon ppackage and add copright info to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,10 @@
+import { MdCopyright } from 'react-icons/md'
+
 export default function Footer() {
-  return <div>Footer</div>
+  return (
+    <div>
+      <MdCopyright />
+      <p>2024 TAG HUNTER</p>
+    </div>
+  )
 }
