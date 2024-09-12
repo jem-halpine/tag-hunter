33b1ec3e5e062ede2fcfe93889d757547fcf0116
TheFlyingDeutchMan <tim.gerschner.nz@gmail.com>
2024-09-13 11:14:25 +1200

    gallery setup

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,4 +1,42 @@
-export default function Gallery() { //Totally not profile ;)
-    return <div>Profile</div>
+import { useQuery } from '@tanstack/react-query'
+import { getAllArtwork } from '@/apis/artworks'
+export default function Gallery() {
+  const {
+    data: artwork,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ['artwork'],
+    queryFn: () => getAllArtwork(),
+  })
+
+  if (isPending) {
+    return <>Loading</>
+  }
+
+  if (isError) {
+    return <>Error</>
+  }
+  
+//   return (
+//     <div>
+//       {artwork?.map((item) => (
+//         <div key={item.id}>
+//           <img src={item.imageUrl} alt={item.title} />
+//           <p>{item.title}</p>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+  return ( // Image from tailwind
+      
+      <div>
+        {artwork.map((item) => {
+          <div key={item.id}>
+          <img src={item.} alt="" />
+        })}
+      </div>
+    )
   }
   
