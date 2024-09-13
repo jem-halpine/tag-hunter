cdfa92843c32d9f33cb68b97462c0d55e92d129d
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-13 15:26:32 +1200

    add styling

--- server/db/index.ts
+++ server/db/index.ts
@@ -17,19 +17,22 @@ export async function getArtworkById(id: number) {
     .first()
 }
 
-export function getArtworks(page: number): Promise<ArtworkData[]> { //getAllArtwork
-  return db('artworks').select(
-    'id',
-    'location',
-    'latitude',
-    'longitude',
-    'artist',
-    'image_url as imageUrl',
-    'user_id as userId',
-    'description',
-  ).limit(5 * page)
+export function getArtworks(page: number): Promise<ArtworkData[]> {
+  //getAllArtwork
+  return db('artworks')
+    .select(
+      'id',
+      'location',
+      'latitude',
+      'longitude',
+      'artist',
+      'image_url as imageUrl',
+      'user_id as userId',
+      'description',
+    )
+    .limit(5 * page)
 }
 
 export function getArtworksLength() {
-  return db('artworks').select().count({count:'*'})
+  return db('artworks').select().count({ count: '*' })
 }
--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -2,8 +2,7 @@ import { useQueryClient, useQuery } from '@tanstack/react-query'
 import { getAllArtwork, getArtworksLength } from '@/apis/artworks'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useState } from 'react'
-
-
+import { Title } from '@/components/Title'
 
 export default function Gallery() {
   const [pageNumber, setPageNumber] = useState(1)
@@ -31,35 +30,35 @@ export default function Gallery() {
     return <>Error</>
   }
   // database.length / 2 = maximumLoad
-  return ( //! DAVID IS THE HERO WITH INFINITE LOADING
-    <InfiniteScroll
-  dataLength={artwork.length} //This is important field to render the next data
-  next={() => {
-    setPageNumber((oldPageNumber) => {
-      queryClient.refetchQueries({queryKey:['artwork']})
-      return(oldPageNumber + 1 )
-    })  
-  }}
-  hasMore = {(artwork.length !== artworkLength.data)}
-  loader={<h4>Loading...</h4>}
-  endMessage={
-    <p style={{ textAlign: 'center' }}>
-      <b>Yay! You have seen it all</b>
-    </p>
-  } //TODO: Get infinite loading to work more smoothly
-  //TODO:  Shoots us up when new content loads from database
-
->
-  <div>
-    {artwork.map((item) => (
-      <div key={item.id}>
-        <img src={`/images/${item.imageUrl}`} alt={item.description} />
-      </div>
-    ))}
-  </div>
-  </InfiniteScroll>
-    
-
-  
+  return (
+    //! DAVID IS THE HERO WITH INFINITE LOADING
+    <div className="ml-12 mr-12 ">
+      <Title title="Gallery"></Title>
+      <InfiniteScroll
+        dataLength={artwork.length} //This is important field to render the next data
+        next={() => {
+          setPageNumber((oldPageNumber) => {
+            queryClient.refetchQueries({ queryKey: ['artwork'] })
+            return oldPageNumber + 1
+          })
+        }}
+        hasMore={artwork.length !== artworkLength.data}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        } //TODO: Get infinite loading to work more smoothly
+        //TODO:  Shoots us up when new content loads from database
+      >
+        <div className="grid grid-cols-3 gap-2">
+          {artwork.map((item) => (
+            <div key={item.id}>
+              <img src={`/images/${item.imageUrl}`} alt={item.description} />
+            </div>
+          ))}
+        </div>
+      </InfiniteScroll>
+    </div>
   )
 }
