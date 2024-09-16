d160a5861f379ac6d03efdae97814323579c52e1
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-16 16:05:04 +1200

    workin gon imgs

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,48 +4,79 @@ import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className="pl-12">
-      <div>
-        <img
-          src="/images/homepage/heart.png"
-          alt="heart"
-          className="left-18 fixed top-96 w-48 rotate-12"
-        />
-        <img
-          src="/images/homepage/spray-can.png"
-          alt="spray can"
-          className="left-18 fixed w-72"
-        />
-        <img
-          src="/images/homepage/spray-can-1.png"
-          alt="spray can"
-          className="fixed right-20 w-48"
-        />
-        <img
-          src="/images/homepage/lightning-bolt.png"
-          alt="lightning bolt"
-          className="fixed bottom-36 right-52 w-28"
-        />
-        <img
-          src="/images/homepage/mural.png"
-          alt="mural"
-          className="fixed bottom-72 right-52 w-56"
-        />
-        <img
-          src="/images/homepage/fire-hydrant.png"
-          alt="fire hydrant"
-          className="fixed right-96 top-28 w-36"
-        />
-        <img
-          src="/images/homepage/skateboard.png"
-          alt="skateboard"
-          className="fixed bottom-72 left-20 w-32"
-        />
-        <img
-          src="/images/homepage/poster.png"
-          alt="poster"
-          className="fixed w-28"
-        />
+    <div className="pl-12 pr-12">
+      <div className="grid max-w-full grid-cols-5 gap-4 overflow-hidden">
+        <div className="h-[35vh]">
+          <img
+            src="/images/homepage/spray-can.png"
+            alt="spray can"
+            className="max-w-[18vw] rotate-12"
+          />
+        </div>
+
+        <div className="h-[35vh]">
+          <img
+            src="/images/homepage/heart.png"
+            alt="heart"
+            className="max-w-[16vw]  -translate-x-[20%] translate-y-[40%] transform"
+          />
+        </div>
+
+        <div className="h-[35vh]">
+          <img
+            src="/images/homepage/skateboard.png"
+            alt="skateboard"
+            className="rotate-14 max-w-[16vw] -translate-x-[20%] transform"
+          />
+        </div>
+
+        <div className="h-[35vh]">
+          <img
+            src="/images/homepage/fire-hydrant.png"
+            alt="fire hydrant"
+            className="max-w-[16vw] translate-x-[10%] translate-y-[10%] rotate-6 transform"
+          />
+        </div>
+
+        <div className="h-[35vh]">
+          <img
+            src="/images/homepage/mural.png"
+            alt="mural"
+            className="max-w-[18vw]"
+          />
+        </div>
+
+        <div className="h-[35vh]">
+          <img
+            src="/images/homepage/graffiti.png"
+            alt="spray can"
+            className="max-w-[16vw] translate-x-[10%] translate-y-[10%] transform"
+          />
+        </div>
+
+        <div className="h-[35vh]">
+          <img
+            src="/images/homepage/lightning-bolt.png"
+            alt="lightning bolt"
+            className="max-w-[12vw] translate-x-[80%] translate-y-[40%] rotate-3 transform"
+          />
+        </div>
+
+        <div className="h-[35vh]">
+          <img
+            src="/images/homepage/mural-1.png"
+            alt="mural"
+            className="max-w-[16vw] translate-x-[80%] translate-y-[15%] transform"
+          />
+        </div>
+
+        <div className="h-[35vh]">
+          <img
+            src="/images/homepage/spray-can-1.png"
+            alt="spray can"
+            className="max-w-[22vw] -translate-y-[15%] translate-x-[60%] -rotate-12 transform"
+          />
+        </div>
       </div>
       <div className="fixed left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col">
         <Link to="/play">
