c5a20cdf62b8d3377504c83a0f26912742b840e0
jem-halpine <29801095+jem-halpine@users.noreply.github.com>
2024-09-17 11:03:22 +1200

    route works with protection

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,6 +1,11 @@
 import connection from './connection'
-import { Artwork, ArtworkPaginate, Game, GameData, UserData } from 'models/models'
-
+import {
+  Artwork,
+  ArtworkPaginate,
+  Game,
+  GameData,
+  UserData,
+} from 'models/models'
 
 const db = connection
 
@@ -46,51 +51,47 @@ export async function getRandomArtwork(): Promise<Artwork> {
 
 export async function getGames(): Promise<Game[]> {
   return await db('games')
-    .join('users','users.auth0Id','=','games.user_id')
+    .join('users', 'users.auth0Id', '=', 'games.user_id')
     .select(
-      "games.id",
-      "games.timestamp",
-      "games.artwork_id as artworkId",
-      "users.name as username",
-      "games.art_was_found as artWasFound",
-      "games.guesses_used as guessesUsed",
+      'games.id',
+      'games.timestamp',
+      'games.artwork_id as artworkId',
+      'users.name as username',
+      'games.art_was_found as artWasFound',
+      'games.guesses_used as guessesUsed',
       // "games.rating",
-    )  
+    )
 }
 
 export async function getLeaderBoard(): Promise<Game[]> {
   return await db('games')
-    .join('users','users.auth0Id','=','games.user_id')
+    .join('users', 'users.auth0Id', '=', 'games.user_id')
     .select('users.name')
     .count('games.id as games')
     .sum('games.art_was_found as wins')
     .sum('guesses_used as guesses')
     .groupBy('users.name')
-
 }
 
 export async function addGame(data: GameData) {
-  
-  const { 
+  const {
     auth0Id: user_id,
     artworkId: artwork_id,
     artWasFound: art_was_found,
     guessesUsed: guesses_used,
-    rating
+    rating,
   } = data
 
   const timestamp = new Date(Date.now())
-  
-  return await db('games').insert(
-    {
-      user_id,
-      artwork_id,
-      art_was_found,
-      guesses_used,
-      rating,
-      timestamp
-    }
-  )
+
+  return await db('games').insert({
+    user_id,
+    artwork_id,
+    art_was_found,
+    guesses_used,
+    rating,
+    timestamp,
+  })
 }
 
 export async function getPaginateArtworks(
@@ -119,4 +120,16 @@ export async function getUserById(auth0id: string): Promise<UserData> {
 
 export async function addUser(user: UserData) {
   return db('users').insert(user).onConflict('auth0id').ignore()
+}
+
+export async function getLeaderBoardByUser(user_id: string): Promise<Game[]> {
+  return await db('users')
+    .leftJoin('games', 'users.auth0Id', '=', 'games.user_id')
+    // .where({ user_id })
+    .where('users.auth0id', user_id)
+    .select('users.name', 'users.email')
+    .count('games.id as games')
+    .sum('games.art_was_found as wins')
+    .sum('guesses_used as guesses')
+    .first()
 }
