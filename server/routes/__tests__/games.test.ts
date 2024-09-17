5ce65218461325b562be20609112de2c1e74c818
Hami Hawkins <hhaami@gmail.com>
2024-09-17 16:28:25 +1200

    Continue games route testing, add notes for where fixing is needed

--- server/routes/__tests__/games.test.ts
+++ server/routes/__tests__/games.test.ts
@@ -21,11 +21,31 @@ afterAll(async () => {
 describe('GET /', () => {
   it('gets all the game data', async () => {
     const res = await request(server).get('/api/v1/games')
+
     expect(res.status).toBe(StatusCodes.OK)
     expect(Array.isArray(res.body)).toBe(true)
+
     expect(res.body[0]).toHaveProperty('id')
     expect(res.body[0]).toHaveProperty('timestamp')
+  })
+  //FIX THIS!!!!!!!
+  it.skip('returns status code 500 if there was an error', async () => {
+    const res = await request(server).get('/api/v1/games')
+
+    expect(res.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
   })
 })
 
 // GET /leaderboard
+describe('GET /api/v1/leaderboard', () => {
+  it('returns leaderboard data successfully', async () => {
+    const res = await request(server).get('/api/v1/games/leaderboard')
+
+    expect(res.status).toBe(StatusCodes.OK)
+    expect(Array.isArray(res.body)).toBe(true)
+    expect(res.body[0]).toHaveProperty('name')
+    expect(res.body[0]).toHaveProperty('guesses')
+  })
+  //FIX THIS!!!!
+  it.skip('returns status code 500 if there is an error', async () => {})
+})
